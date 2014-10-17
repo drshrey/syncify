ba60d31ff44167d82d826a20f1372629dee53c59
Shreyas Jaganmohan <shreyas.jaganmohan@gmail.com>
2014-10-17 03:18:47 -0400

    changed syncobject to have comments

--- sync_object.py
+++ sync_object.py
@@ -2,15 +2,25 @@ class Syncify(object):
 	def __init__(self, track_name, auth=None):
 		self.track_name = track_name
 		self.found = False
-	
+
+	'''Return the track name of this node
+	'''
 	def get_track_name():
 		return self.track_name
 	
+	'''Return whether this node has been found in Spotify. 
+	'''
 	def is_found():
 		return self.found
 
+	'''Sets true or false to whether the 
+	   node's track has been found inside Spotify
+
+	'''
 	def set_found(yes_or_no):
 		self.found = yes_or_no
 	
+	'''Sets the track name for the node. 
+	'''
 	def set_track_name(track_name):
 		self.track_name = track_name
